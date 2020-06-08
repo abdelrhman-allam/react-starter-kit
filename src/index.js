c1866c1dafcaa9bec9babfaa42cd5ca43ab2d1e6
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-06-08 22:03:36 +0200

    Redux Structer and Connect React

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={configureStore}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
