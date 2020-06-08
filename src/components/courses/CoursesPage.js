cce1619184caa488402759ad653e86ef5281710a
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-06-08 22:56:34 +0200

    MapDispatchToProps simplify

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -16,7 +16,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   };
 
   render() {
@@ -42,6 +42,7 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   dispatch: PropTypes.func.isRequired,
+  createCourse: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -50,4 +51,9 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: (course) => dispatch(courseActions.createCourse(course)),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
