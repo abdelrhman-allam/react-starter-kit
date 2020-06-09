9731a2aad362fbf77742a694b5017aa79e8ddfe6
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-06-09 17:01:46 +0200

    Load and Render Data

---
+++ src/components/courses/CourseList.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export default function CourseList(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th />
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.courses.map((course) => {
+          return (
+            <tr key={course.id}>
+              <td>
+                <a href={"https://pluralsight.com/courses/" + course.slug}>
+                  Watch
+                </a>
+              </td>
+              <td>
+                <Link to={"/course/" + course.slug}>{course.title}</Link>
+              </td>
+              <td>{course.authorName}</td>
+              <td>{course.category}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+CourseList.propTypes = {
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
