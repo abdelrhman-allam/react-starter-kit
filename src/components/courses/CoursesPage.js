d77252678c5b8f0b7b771503d5ec01348bacd8d5
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-06-09 15:24:19 +0200

    Load Courses from Mock API

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -4,38 +4,19 @@ import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: "",
-    },
-  };
-
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
+  componentDidMount() {
+    this.props.actions.loadCourses().catch((error) => {
+      console.log("Error loading: " + error);
+    });
+  }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Courses</h3>
-        <input
-          type="text"
-          value={this.state.course.title}
-          onChange={this.handleChange.bind(this)}
-        />
-        <input type="submit" value="save" />
-
         {this.props.courses.map((course) => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
