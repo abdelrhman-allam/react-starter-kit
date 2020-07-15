1a781ca9fa560af1edfc4975802b619d67e62baa
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-07-15 13:03:23 +0200

    Update configureStore.prod.js

--- src/redux/configureStore.prod.js
+++ src/redux/configureStore.prod.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
+// main store register rooReducers (main load all reducers) for thunk middleware 
 export default function configureStore(initialState) {
   return createStore(rootReducer, initialState, applyMiddleware(thunk));
 }
